986b9a0d50346944d760be5ce0ce3ed1ab691a40
kotahorii <khorii.micron@gmail.com>
2021-11-12 15:08:21 +0900

    serializers add

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,14 +2,14 @@ class Api::V1::UsersController < ApplicationController
   before_action :set_user, except:[:index, :create]
 
   def index
-    users = User.all
-    render status: 200, json: { data: users }
+    @users = User.all
+    render json: @users, each_serializer: UserSerializer
   end
 
   def create
     user = User.new(user_params)
     if user.save
-      render status: 200, json: { data: user }
+      render status: 200, json: user, serializer: UserSerializer
     else
       render status: 400, json: { data: user.errors }
     end
@@ -21,7 +21,7 @@ class Api::V1::UsersController < ApplicationController
 
   def update
     if @user.update(user_params)
-      render status: 200, json: { data: @user }
+      render status: 200, json: @user, serializer: UserSerializer
     else
       render status: 400, json: { data: @user.errors }
     end
--- config/initializers/ams.rb
+++ config/initializers/ams.rb
@@ -1,2 +1,1 @@
 ActiveModelSerializers.config.default_includes = '**'
-ActiveModelSerializers.config.adapter = :json_api
